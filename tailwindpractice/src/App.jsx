a512a73fc84951122b64b1bdf3a7d3092e7a8205
Ghufran <ghugluq2t@gmail.com>
2026-07-14 17:57:42 +0100

    .

--- tailwindpractice/src/App.jsx
+++ tailwindpractice/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
-  return <div>
-      <p className="text-xl">Hello World</p>
-    </div>;
+  return (
+    <div>
+      <p className="text-xl font-semibold text-center">Hello World</p>
+    </div>
+  );
 }
 
 export default App
