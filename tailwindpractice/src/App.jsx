0f37e3f3f6509d1b84a858b850dd2bf7ec08c8d7
Ghufran <ghugluq2t@gmail.com>
2026-07-14 18:36:32 +0100

    basic grid design, before responsiveness

--- tailwindpractice/src/App.jsx
+++ tailwindpractice/src/App.jsx
@@ -1,12 +1,24 @@
 function App() {
   return (
-    <div className="flex items-center justify-between bg-slate-900 text-white p-4">
+    <div>
 
-      <div className="font-bold">Logo</div>
-      <div className="flex gap-2">
-        <span>Home</span>
-        <span>About</span>
-        <span>Contact</span>
+      <div className="flex items-center justify-between bg-slate-900 text-white p-4">
+
+        <div className="font-bold">Logo</div>
+        <div className="flex gap-2">
+          <span>Home</span>
+          <span>About</span>
+          <span>Contact</span>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 text-white p-5 gap-4">
+        <div className="bg-slate-500 p-3 rounded">Feature One</div>
+        <div className="bg-slate-500 p-3 rounded">Feature Two</div>
+        <div className="bg-slate-500 p-3 rounded">Feature Three</div>
+        <div className="bg-slate-500 p-3 rounded">Feature Four</div>
+        <div className="bg-slate-500 p-3 rounded">Feature Five</div>
+        <div className="bg-slate-500 p-3 rounded">Feature Six</div>
       </div>
     </div>
   );
