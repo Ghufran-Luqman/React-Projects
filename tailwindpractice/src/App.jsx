69be97c5701e3ef3ac603dd818fb52dac9cfc3b2
Ghufran <ghugluq2t@gmail.com>
2026-07-14 18:15:24 +0100

    Some basic styling

--- tailwindpractice/src/App.jsx
+++ tailwindpractice/src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
   return (
     <div>
-      <p className="text-xl font-semibold text-center">Hello World</p>
+
+      <div className="bg-amber-500 p-5">
+        <p className="text-xl font-semibold text-center text-rose-700">
+          Hello World
+         </p>
+      </div>
     </div>
   );
 }
