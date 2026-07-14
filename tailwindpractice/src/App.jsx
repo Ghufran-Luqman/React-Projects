1b5210038c479b82997502dc5f8ee6fb0d083805
Ghufran <ghugluq2t@gmail.com>
2026-07-14 19:23:25 +0100

    Added dark and light mode with button (don't understand this react syntax)

--- tailwindpractice/src/App.jsx
+++ tailwindpractice/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 
 function App() {
   const [open, setOpen] = useState(false);
+  const [theme, setTheme] = useState("light");
   return (
-    <div>
+    <div className={theme}>
       {/* Navbar */}
-      <div className="flex items-center justify-between bg-slate-900 text-white p-4">
+      <div className="flex items-center justify-between text-slate-900 dark:bg-slate-900 dark:text-white p-4">
         <div className="font-bold">Logo</div>
 
         {/* Desktop Nav */}
@@ -20,20 +21,24 @@ function App() {
       
       {/* Mobile Nav */}
         {open && ( 
-          <div className="flex flex-col items-center p-3 gap-2 bg-slate-900 text-white sm:hidden">
+          <div className="flex flex-col items-center p-3 gap-2 text-slate-900 dark:bg-slate-900 dark:text-white sm:hidden">
             <span>Home</span>
             <span>About</span>
             <span>Contact</span>
+            <button className="sm:hidden cursor-pointer" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+              {theme === "light" ? "☾" : "☼" }
+            </button>
           </div>
         )}
+        
 
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 text-white p-5 gap-4 text-center font-semibold text-2xl sm:text-lg">
-        <div className="bg-slate-500 p-3 rounded hover:bg-slate-600 hover:scale-102 transition-all duration-300">Feature One</div>
-        <div className="bg-slate-500 p-3 rounded hover:bg-slate-600 hover:scale-102">Feature Two</div>
-        <div className="bg-slate-500 p-3 rounded hover:bg-slate-600">Feature Three</div>
-        <div className="bg-slate-500 p-3 rounded hover:bg-slate-600">Feature Four</div>
-        <div className="bg-slate-500 p-3 rounded hover:bg-slate-600">Feature Five</div>
-        <div className="bg-slate-500 p-3 rounded hover:bg-slate-600">Feature Six</div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 dark:bg-slate-700 dark:text-slate-700 text-white p-5 gap-4 text-center font-semibold text-2xl sm:text-lg">
+        <div className="bg-slate-500 dark:bg-slate-200 p-3 rounded hover:bg-slate-600 dark:hover:bg-slate-300 hover:scale-102 transition-all duration-300">Feature One</div>
+        <div className="bg-slate-500 dark:bg-slate-200 p-3 rounded hover:bg-slate-600 dark:hover:bg-slate-300 hover:scale-102">Feature Two</div>
+        <div className="bg-slate-500 dark:bg-slate-200 p-3 rounded hover:bg-slate-600 dark:hover:bg-slate-300">Feature Three</div>
+        <div className="bg-slate-500 dark:bg-slate-200 p-3 rounded hover:bg-slate-600 dark:hover:bg-slate-300">Feature Four</div>
+        <div className="bg-slate-500 dark:bg-slate-200 p-3 rounded hover:bg-slate-600 dark:hover:bg-slate-300">Feature Five</div>
+        <div className="bg-slate-500 dark:bg-slate-200 p-3 rounded hover:bg-slate-600 dark:hover:bg-slate-300">Feature Six</div>
       </div>
     </div>
   );
